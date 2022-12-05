6c0c8adba882b59aba4e1cf5cda57cacd951fa73
RodAvila <93148064+RodAvila@users.noreply.github.com>
2022-12-05 16:22:08 -0500

    Add files via upload
    HashMap Separate Chaining

---
+++ HashMap.java
@@ -0,0 +1,54 @@
+// Referenced https://www.geeksforgeeks.org/method-class-hashcode-method-in-java/
+
+import java.math.BigInteger;
+
+public class HashMap {
+
+
+    Map[] hashMap;
+    int size;
+    double loadFactor;
+
+    public HashMap()
+    {
+        this.hashMap = new Map[16];
+        this.size = 0;
+        this.loadFactor = 0.75;
+    }
+
+    public void insert(String channelID, String channelTitle, String videoTitle, String publishDate, BigInteger views, String videoID, BigInteger likeCount, BigInteger dislikeCount)
+    {
+
+        //check for load factor size / hashMap.length == loadFactor
+        if ((double)size / (double)hashMap.length == loadFactor)
+        {
+            reSize();
+        }
+
+        //get index from hash function and mod it by hashMap.length
+        int index = videoID.hashCode() % hashMap.length;
+
+        //check if hashMap[index] is empty, insert and increase size by 1
+        if (hashMap[index] == null)
+        {
+            hashMap[index] = new Map();
+        }
+
+        //Insert into already existing RB tree and increase size by 1
+        hashMap[index].insert(channelID, channelTitle, videoTitle, publishDate, views, videoID, likeCount, dislikeCount);
+        size++;
+    }
+
+    private void reSize()
+    {
+        Map[] temp = hashMap;
+        hashMap = new Map[hashMap.length * 2];
+
+        for (int i = 0; i < temp.length; i++)
+        {
+            hashMap[i] = temp[i];
+        }
+    }
+
+
+}
